6bd97c2bcc4b949d77b10f733eb9859f234505b1
tuan882612 <tuan882612@gmail.com>
2023-04-22 23:54:40 -0500

    chore: made base handler more verbose and updated double qoutes

--- src/api/endpoints/base.py
+++ src/api/endpoints/base.py
@@ -1,18 +1,25 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 from typing import Dict, Any
+from motor.motor_asyncio import AsyncIOMotorDatabase
+from src.domain.user.model import User
 
 from src.infastructure.config.environment.base import settings
+from src.infastructure.database.mongo import get_client
 
 router = APIRouter()
 
-@router.get("/health")
-async def health_handler(req: Request) -> Dict[str, Any]:
+@router.get('/health')
+async def health_handler() -> Dict[str, Any]:
     return {
-        "status": "OK",
-        "version": settings.VERSION,
-        "dependencies": [],
+        'status': 'OK',
+        'version': settings.VERSION,
+        'dependencies': [],
     }
 
-@router.get("/")
-async def root_handler() -> Dict[str, str]:
-    return {"message": "base endpoint"}
+@router.get('/')
+async def root_handler() -> Dict[str, Any]:
+    return {
+        'code': 200,
+        'message': 'Base endpoint',
+        'body': {}
+    }
