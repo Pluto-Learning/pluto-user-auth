7b97f4fa4c2297e22dfec2b656e580d986e446ef
tuan882612 <tuan882612@gmail.com>
2023-04-23 12:18:46 -0500

    chore: refactor init_app for readablility

--- src/api/server.py
+++ src/api/server.py
@@ -3,7 +3,8 @@ from fastapi import APIRouter, FastAPI
 from src.infastructure.config.environment.base import settings
 from src.api.endpoints import base, user
 
-def load_routes(router: APIRouter) -> APIRouter:
+def load_routes() -> APIRouter:
+    router: APIRouter = APIRouter(prefix=settings.API_PREFIX)
     router.include_router(router=base.router)
     router.include_router(router=user.router, prefix='/user')
     return router
@@ -14,9 +15,8 @@ def init_api() -> FastAPI:
         version=settings.VERSION,
         description=settings.DESCRIPTION
     )
-    
-    api_router = APIRouter(prefix=settings.API_PREFIX)
-    app_router = load_routes(api_router)
+
+    app_router = load_routes()
     app.include_router(router=app_router)
     
     return app
