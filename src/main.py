680e801a7930b5015be59a13612c139eb5c78385
tuan882612 <tuan882612@gmail.com>
2023-04-23 17:06:45 -0500

    feat: store db in state allow for 11 times performance increase

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorDatabase, AsyncIOMotorClient
 import multiprocessing
 import uvicorn
 
 from src.api.server import init_api
+from src.infastructure.database.mongo import get_client
 
 app: FastAPI = init_api()
+
+@app.on_event("startup")
+async def startup_event():
+    app.client: AsyncIOMotorClient = await get_client()
+    app.db: AsyncIOMotorDatabase = app.client['user-auth']
+    
+@app.on_event("shutdown")
+def shutdown_event():
+    app.client.close()
 
 if __name__=="__main__":
     uvicorn.run(
